b893b50494dac6a1bf08dfaa5e16019c5ce52381
arashik65 <mdashikur6398+@gmail.com>
2026-02-12 14:18:16 +0600

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+const products =[
+    {name:'phone',price: 12000},
+    {name:'laptop', price: 32000},
+]
+console.log(products[0]);
+console.log(products[1]);
+
+const products1={
+    '0': 15,
+    '1':13,
+    '2':87,
+}
+console.log(products1[0]);
