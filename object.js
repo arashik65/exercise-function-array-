6b68e8721b960129a0323f512d5ed48629421e11
arashik65 <mdashikur6398+@gmail.com>
2026-02-12 14:08:36 +0600

    object example array function

---
+++ object.js
@@ -0,0 +1,28 @@
+const nayok={
+    name:'sakib khan',
+    id: 121,
+    address: 'movie cinema',
+    isSingle: true,
+    friends: ['Jahid','Jobbar','Ashik','rup','Amzad'],
+    movies:[{name:'no-1', year: 2015},{name:'king khan',year:2018}],
+    act: function(){
+        console.log('acting like sakib khan');
+    },
+    car:{
+        brand:'tesla',
+        price:500000,
+        made:2025,
+        manufacture:{
+            name:'tesla',
+            ceo: 'elon mask',
+            country: 'USA'
+        }
+    }
+}
+
+
+
+console.log(nayok);
+console.log(nayok.movies);
+console.log(nayok.act);
+nayok.act();
